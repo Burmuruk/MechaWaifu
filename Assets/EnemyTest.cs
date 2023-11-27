9b44285af324ef535bdaf88cce6da1293f7c53b7
Burmuruk <santoyo.herrera.david@gmail.com>
2023-11-26 22:50:11 -0600

    Mejora de camara

--- Assets/EnemyTest.cs
+++ Assets/EnemyTest.cs
@@ -2,13 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyTest2 : MonoBehaviour
+public class EnemyTest2 : Character
 {
-    
+    [Header("Behaviour")]
+    [SerializeField] float minDistance = 10;
+    Player player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        player = FindObjectOfType<Player>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var dis = Vector3.Distance(transform.position, player.transform.position);
+
+        if (dis > minDistance)
+        {
+            MoveTo(player.transform.position);
+        }
+
+        if (Ammo > 0)
+        {
+            attacks = Attack.shoot;
+            Attacking();
+        }
     }
 }
